d61868059a3a68a4e6873c4e4ee23ad2119e79c6
MarineVega <marinev33@hotmail.com>
2024-05-28 22:20:57 -0300

    terminada la validación de login e inicio de sesión

--- js/validarLogin.js
+++ js/validarLogin.js
@@ -1,48 +1,52 @@
+function esValidoEmail(email) {
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+}
+
+
+function mostrarMensajeError(elementId, mensaje) {
+    var errorElemento = document.getElementById(elementId);    
+    errorElemento.innerText = mensaje;
+}
+
 function resetMesajesError(){
     // levanto todos los elementos que tengan la clase mensaje-error
-    let errorElementos = document.querySelectorAll(".mensaje-error");
+    let errorElementos = document.querySelectorAll('.mensaje-error');
     errorElementos.forEach(function(elemento) {
-        elemento.innerText = " ";
+        elemento.innerText = "";
     });
 }
 
-function mostrarMensajeError(elementId, mensaje) {
-    let errorElemento = document.getElementById(elementId);
-    errorElemento.innerText = mensaje;
-}
-    
-
-document.addEventListener("DOMContentLoaded", ()=>{
-    const form = document.getElementById("loginForm");
-    console.log(form);
-    form.addEventListener("submit", (event)=>{
+const validarFormulario = () => {
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
         // Evito que se envíe el formulario automáticamente
         event.preventDefault();
-
+    
         // Reseteo los mensajes de error
         resetMesajesError();
-
+        
         // Valido los campos
-        let email = document.getElementById("email").value.trim();  //trim elimina espacios
-        let contrasenia = document.getElementById("contrasenia").value.trim();
+        let email = document.getElementById('email').value.trim();  //trim elimina espacios
+        let contrasenia = document.getElementById('contrasenia').value.trim();
         let esValido = true;
-
-      //  if (!esValidoEmail(email)) {
+      
         if (email === "") {
             mostrarMensajeError("emailError", "Por favor ingrese un correo electrónico válido.");
             esValido = false;
         }
-
-        if (contrasenia.length > 8) {
+        
+        if (contrasenia.length < 8) {        
             mostrarMensajeError("contraseniaError", "La contraseña debe tener al menos 8 caracteres.");
             esValido = false;
         }
 
         if (esValido) {
-            alert("¡Formulario enviado correctamente!");
+            alert('¡Formulario enviado correctamente!');
         }
-    });
-} );
+    });  
+};
+
+document.addEventListener("DOMContentLoaded", validarFormulario);
 
 
 
